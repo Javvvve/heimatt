05c6128d42eadfbf7c8b4df75b10a31fe7698f35
Javvvve <1099046808@qq.com>
2020-03-04 01:18:12 +0800

    频道下拉

--- src/views/home/main/home.vue
+++ src/views/home/main/home.vue
@@ -2,15 +2,17 @@
   <div class="home">
     <van-search v-model="value" shape="round" background="#3296fa" placeholder="请输入搜索关键词"
     class="searchBar"/>
-    <van-tabs v-model="active" @change="onLoad" class="tabs">
+    <van-tabs v-model="active" @change="channelChange" class="tabs">
       <van-tab v-for="(item, index) in channels" :key="index" :title="item.name" :name="item.id">
       </van-tab>
     </van-tabs>
+    <van-pull-refresh v-model="listState.refreshing" @refresh="onRefresh">
     <van-list
       v-model="listState.loading"
       :finished="listState.finished"
       finished-text="没有更多了"
       @load="onLoad"
+      offset="20"
       class="list"
     >
       <van-cell-group>
@@ -18,6 +20,7 @@
         :title="item.title" label="描述信息" />
       </van-cell-group>
     </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -33,18 +36,37 @@ export default {
       articleList: [],
       listState: {
         loading: false,
-        finished: true,
+        finished: false,
+        refreshing: false,
       },
     };
   },
   methods: {
     async getArticleList() {
       const res = await getArticle(this.active);
+      if (res.data.data.results.length < 10) {
+        this.listState.finished = true;
+      }
       return res.data.data.results;
     },
     async onLoad() {
-      this.articleList = await this.getArticleList();
+      const articleData = await this.getArticleList();
+      this.articleList = [...this.articleList, ...articleData];
       this.listState.loading = false;
+    },
+    async onRefresh() {
+      this.articleList = await this.getArticleList();
+      this.listState.refreshing = false;
+      this.listState.loading = false;
+    },
+    channelChange() {
+      this.listState = {
+        loading: false,
+        finished: false,
+        refreshing: false,
+      };
+      this.articleList = [];
+      this.onLoad();
     },
   },
   async created() {
@@ -65,9 +87,9 @@ export default {
         position: fixed;
         width: 100%;
         top: 54px;
-        z-index: 10000;
+        z-index: 9999;
     }
     .list{
-        padding-top: 98px;
+        margin-top: 98px;
     }
 </style>
